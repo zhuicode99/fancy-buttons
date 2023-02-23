b04d817df29139783b37d53de168128c052997ed
zhuicode99 <enlin291814833@hotmail.com>
2023-02-23 15:44:58 -0500

    add four buttons

--- src/App.js
+++ src/App.js
@@ -6,9 +6,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button className="AngryButton">
+    {/* When the threshold is not reached */}<span>Don't click me too much! </span>
+    {/* When the threshold is not reached */}<span>Rawr!</span>
+  </button>
+  <button className="CounterButton">
+      You clicked me X amount of times
+  </button>
+  <button className="LightSwitchButton">
+      {/* When the state is on */}<span><i>💡</i> I'm on!</span>
+      {/* When the state is off */}<span className="off"><i>💡</i> I'm off!</span>
+    </button>
+    <button className="TextRepeaterButton">
+      <span>I like this text</span>
+  </button>
         <a
           className="App-link"
           href="https://reactjs.org"
